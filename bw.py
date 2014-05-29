b442fc60d1a136bff2fdd956a398cad9a20d9996
Piotr Kaleta <piotrek.kaleta@gmail.com>
2014-05-29 15:59:06 +0200

    Proper HMM structure, increased precision of calculations.

--- bw.py
+++ bw.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+from mpmath import mpf
 
 from gen import gen_seq
 
@@ -16,14 +17,14 @@ def forward(hmm_params, X, Y, Yt):
     A, B, pi = hmm_params
     n = len(Yt)
     m = len(X)
-    alpha = np.zeros((n, m))
+    alpha = np.zeros((n, m), dtype=mpf)
 
     for t in xrange(n):
         for i in xrange(m):
             if t == 0:
-                alpha[t, i] = pi[i] * B[i, Yt[t]]
+                alpha[t, i] = mpf(pi[i] * B[i, Yt[t]])
             else:
-                tmp = 0.0
+                tmp = mpf(0.0)
                 for j in xrange(m):
                     tmp += alpha[t - 1, j] * A[j, i]
                 alpha[t, i] = B[i, Yt[t]] * tmp
@@ -35,12 +36,12 @@ def backward(hmm_params, X, Y, Yt):
     A, B, pi = hmm_params
     n = len(Yt)
     m = len(X)
-    beta = np.zeros((n, m))
+    beta = np.zeros((n, m), dtype=mpf)
 
     for t in xrange(n - 1, -1, -1):
         for i in xrange(m):
             if t == n - 1:
-                beta[t, i] = 1.0
+                beta[t, i] = mpf(1.0)
             else:
                 for j in xrange(m):
                     beta[t, i] += beta[t + 1, j] * A[i, j] * B[j, Yt[t + 1]]
@@ -66,16 +67,18 @@ def baum_welch(hmm_params, X, Y, obs, iterations=1):
             alpha = forward(hmm_params, X, Y, Yt)
             beta = backward(hmm_params, X, Y, Yt)
 
-            gamma = np.zeros((n, m))
-            gamma_sums = np.zeros(n)
+            gamma = np.zeros((n, m), dtype=mpf)
+            gamma_sums = np.zeros(n, dtype=mpf)
             for t in xrange(n):
                 for i in xrange(m):
-                    gamma[t, i] = alpha[t, i] * beta[t, i]
-                    gamma_sums[t] += gamma[t, i]
+                    gamma[t, i] = mpf(alpha[t, i] * beta[t, i])
+                    gamma_sums[t] += mpf(gamma[t, i])
 
+                #import ipdb; ipdb.set_trace()
+                #print gamma_sums[t]
                 gamma[t] /= gamma_sums[t]
 
-            xsi = np.zeros((n, m, m))
+            xsi = np.zeros((n, m, m), dtype=mpf)
             for t in xrange(n - 1):
                 for i in xrange(m):
                     for j in xrange(m):
@@ -87,7 +90,7 @@ def baum_welch(hmm_params, X, Y, obs, iterations=1):
             # Update
             pi = gamma[0]
 
-            A = np.zeros((m, m))
+            A = np.zeros((m, m), dtype=mpf)
             for i in xrange(m):
                 den = 0.0
                 for t in xrange(n - 1):
@@ -101,7 +104,7 @@ def baum_welch(hmm_params, X, Y, obs, iterations=1):
                     else:
                         A[i, j] = num / den
 
-            B = np.zeros((m, len(Y)))
+            B = np.zeros((m, len(Y)), dtype=mpf)
             for i in xrange(len(Y)):
                 for j in xrange(m):
                     tmp = 0.0
@@ -123,7 +126,7 @@ def forward_backward(hmm_params, X, Y, sequence):
     seq_indices = [Y.index(yt) for yt in sequence]
     alpha = forward(hmm_params, X, Y, seq_indices)
     beta = backward(hmm_params, X, Y, seq_indices)
-    prob = np.zeros((n, m))
+    prob = np.zeros((n, m), dtype=mpf)
     for t in xrange(n):
         for i in xrange(m):
             prob[t, i] = alpha[t, i] * beta[t, i]
@@ -136,23 +139,23 @@ def forward_backward(hmm_params, X, Y, sequence):
 #if __name__ == '__main__':
 transition_prob = np.matrix([
     [0.0, 1.0, 0.0, 0.0],
-    [0.0, 1-0.0001, 0.0001, 0.0],
-    [0.0, 0.0, 1-0.0001, 0.0001],
-    [0.0, 0.0, 0.0, 0.0]])
+    [0.0, 1 - 0.0002, 0.0002, 0.0],
+    [0.0, 0.0, 1 - 0.0002, 0.0002],
+    [0.0, 0.0, 0.0, 0.0]], dtype=mpf)
 
 emission_prob = np.matrix([
-    [1.0, 0.0, 0.0, 0.0],
-    [0.0, 0.95, 0.05, 0.0],
-    [0.0, 0.05, 0.95, 0.0],
-    [0.0, 0.0, 0.0, 1.0]])
+    [1.0, 0.0, 0.0, 0.0, 0.0],
+    [0.0, 0.96, 0.036, 0.004, 0.0],
+    [0.0, 0.96, 0.004, 0.036, 0.0],
+    [0.0, 0.0, 0.0, 0.0, 1.0]], dtype=mpf)
 
-initial_prob = np.array([1.0, 0.0, 0.0, 0.0])
+initial_prob = np.array([1.0, 0.0, 0.0, 0.0], dtype=mpf)
 
 with open('seq.txt') as fp:
     sequences = [line.strip() for line in fp.readlines()]
 
-hidden_states = ['start', 'before', 'after', 'end']
-symbols = ['S', 'L', 'R', 'E']
+hidden_states = ['S0', 'S1', 'S2', 'S3']
+symbols = ['S', 'N', 'L', 'R', 'E']
 
 hmm_params = baum_welch(
     HMMParams(
